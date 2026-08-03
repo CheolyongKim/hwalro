596c93881e130eeb881813c6e10dc51de0cb7905
jhj2075 <flg989@naver.com>
2026-08-03 12:25:49 +0900

    feat(SCRUM-7): 공통 레이아웃 사이드바 구현

--- apps/frontend/src/router/index.tsx
+++ apps/frontend/src/router/index.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import App from '../App';
+import WorkspaceLayout from '../layouts/WorkspaceLayout';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 
@@ -8,8 +9,11 @@ export const router = createBrowserRouter([
     path: '/',
     element: <App />,
     children: [
-      { index: true, element: <HomePage /> },
       { path: 'login', element: <LoginPage /> },
+      {
+        element: <WorkspaceLayout />,
+        children: [{ index: true, element: <HomePage /> }],
+      },
     ],
   },
 ]);
